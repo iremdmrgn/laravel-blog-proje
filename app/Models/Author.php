c4162ef30965c5958a412966e3f198381a3e89f1
İrem Demireğen <irem-demiregen@hotmail.com>
2025-09-08 21:57:29 +0300

    updated

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    // Bir yazarın birden fazla blogu olabilir
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -12,5 +12,19 @@ class Blog extends Model
     protected $fillable = [
         'title',
         'content',
+        'status',
+        'author_id',
     ];
+
+    // Bir blog bir yazara ait
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    // Bir blog birden fazla kategoriye ait
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
