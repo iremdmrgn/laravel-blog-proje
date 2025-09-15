f5b8123b84fac9d7e30112f2bf78704b7a37864e
İrem Demireğen <irem-demiregen@hotmail.com>
2025-09-15 17:54:39 +0300

    update

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app') {{-- Ana layout kullanıyorsan --}}
+@extends('layouts.app') 
 @section('content')
 <div class="container" style="padding: 20px;">
     <h1 style="text-align:center; margin-bottom:30px;">Bloglar</h1>
